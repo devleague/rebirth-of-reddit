63d3336426b9f799a050a6706488c2a1f2df1d6c
sgalutira <sheena.galutira@gmail.com>
2016-06-10 22:41:51 -1000

    created a button. Need eventlistener

--- app.js
+++ app.js
@@ -18,38 +18,46 @@ var posts = $.ajax({
 });
 
 
-
-    // var redditData = posts.responseJSON;
+    var redditContainer = document.getElementById('reddit');
+    var buttonDiv = document.createElement('div');
+    buttonDiv.className = "button";
+    var fetchButton = document.createElement('BUTTON');
+    var buttonText = document.createTextNode("Fetch Post");
+    fetchButton.appendChild(buttonText);
+    buttonDiv.appendChild(fetchButton);
+    var mainContainer = document.createElement('div');
+    mainContainer.className = "subreddit";
+    redditContainer.appendChild(buttonDiv);
+    redditContainer.appendChild(mainContainer);
 
 
   function newPosts(response){
-  var redditContainer = document.getElementById('reddit');
-  var mainContainer = document.createElement('div');
-  mainContainer.className = "subreddit";
-  redditContainer.appendChild(mainContainer);
-
-  var results = response.data.children;
+    var results = response.data.children;
 
     for (var i = 0; i < results.length; i++){
-    var author = results[i].data.author;
-    var title = results[i].data.title;
-    var forumText = results[i].data.selftext;
+      var author = results[i].data.author;
+      var title = results[i].data.title;
+      var forumText = results[i].data.selftext;
 
-    var headerDiv = document.createElement('div');
-    headerDiv.className = "title";
-    headerDiv.innerHTML = title;
-    mainContainer.appendChild(headerDiv);
+      var headerDiv = document.createElement('div');
+      headerDiv.className = "title";
+      headerDiv.innerHTML = title;
+      mainContainer.appendChild(headerDiv);
 
-    var bodyTextDiv = document.createElement('div');
-    bodyTextDiv.className = "forumText";
-    bodyTextDiv.innerHTML = forumText;
-    mainContainer.appendChild(bodyTextDiv);
+      var authorDiv = document.createElement('div');
+      authorDiv.className = "author";
+      authorDiv.innerHTML = "author";
+      mainContainer.appendChild(authorDiv);
 
-    var authorDiv = document.createElement('div');
-    authorDiv.className = "author";
-    authorDiv.innerHTML = "author";
-    mainContainer.appendChild(authorDiv);
+      var bodyTextDiv = document.createElement('div');
+      bodyTextDiv.className = "forumText";
+      bodyTextDiv.innerHTML = forumText;
+      mainContainer.appendChild(bodyTextDiv);
+
 
   }
   return mainContainer;
 }
+
+    fetchButton.addEventListener('click', newPosts);
+

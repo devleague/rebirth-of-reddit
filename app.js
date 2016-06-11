514b947ec407d50d44e280a97c44eb493564b3b5
sgalutira <sheena.galutira@gmail.com>
2016-06-11 13:38:18 -1000

    before making branch

--- app.js
+++ app.js
@@ -25,39 +25,55 @@ var posts = $.ajax({
     // var buttonText = document.createTextNode("Fetch Post");
     // fetchButton.appendChild(buttonText);
     // buttonDiv.appendChild(fetchButton);
-    var mainContainer = document.createElement('div');
-    mainContainer.className = "subreddit";
     // redditContainer.appendChild(buttonDiv);
-    redditContainer.appendChild(mainContainer);
 
 
   function newPosts(response){
     var results = response.data.children;
-
     for (var i = 0; i < results.length; i++){
       var author = results[i].data.author;
       var title = results[i].data.title;
-      var forumText = results[i].data.selftext;
+      //if forumText is truthy will return the forumText, if falsey returns title
+      var forumText = results[i].data.selftext ? results[i].data.selftext : title;
+      var numComments = results[i].data.num_comments;
+      var postDiv = postData('div', 'posts', null);
+      redditContainer.appendChild(postDiv);
 
-      var headerDiv = document.createElement('div');
-      headerDiv.className = "title";
-      headerDiv.innerHTML = title;
-      mainContainer.appendChild(headerDiv);
+      var headerDiv = postData('a', 'title', title);
+      var titleLink = document.getElementsByClassName('title');
+        titleLink[i].setAttribute('href', results[i].data.url);
+      postDiv.appendChild(headerDiv);
 
-      var authorDiv = document.createElement('div');
-      authorDiv.className = "author";
-      authorDiv.innerHTML = author;
-      mainContainer.appendChild(authorDiv);
+      var bodyTextDiv = postData('div', 'forumText', forumText);
+      postDiv.appendChild(bodyTextDiv);
 
-      var bodyTextDiv = document.createElement('div');
-      bodyTextDiv.className = "forumText";
-      bodyTextDiv.innerHTML = forumText;
-      mainContainer.appendChild(bodyTextDiv);
+      var dateDiv = postData('div', 'date', redditDate(results));
+      postDiv.appendChild(dateDiv);
 
+      var authorDiv = postData('div', 'author', author);
+      postDiv.appendChild(authorDiv);
 
+      var numCommentDiv = postData('div', 'numComments', numComments);
+      postDiv.appendChild(numCommentDiv);
   }
-  return mainContainer;
+  return redditContainer;
 }
 
     // fetchButton.addEventListener('click', newPosts);
 
+  var redditDate = function(theDate){
+    for (var i = 0; i < theDate.length; i++){
+    var date = theDate[i].data.created_utc;
+    var utcDate = new Date(date * 1000);
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return days[utcDate.getUTCDay()] + ' ' + month[utcDate.getUTCMonth()] + ' ' + utcDate.getUTCDate(date) + ', ' + utcDate.getUTCFullYear(date);
+    }
+  };
+
+  function postData(element, className, text){
+    var el = document.createElement(element);
+    el.className = className;
+    el.innerHTML = text;
+    return el;
+  }

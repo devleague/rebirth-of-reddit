0db31aefb0f4db388e8a8a780db0f32cc5e16cbf
Christie R <creindle@hawaii.edu>
2016-06-11 13:57:50 -1000

    Finished setting up files

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');//gulp was a saved as a dependency
+/*
+⇒  gulp
+hi
+[13:52:32] Using gulpfile ~/Desktop/DevLeague/rebirth-of-reddit/gulpfile.js
+[13:52:32] Task 'default' is not in your gulpfile
+[13:52:32] Please check the documentation for proper gulpfile formatting
+*/
+gulp.task('default', function(){
+  console.log("HIIII");
+});
+
+/*
+⇒  gulp
+[13:54:06] Using gulpfile ~/Desktop/DevLeague/rebirth-of-reddit/gulpfile.js
+[13:54:06] Starting 'default'...
+HIIII
+[13:54:06] Finished 'default' after 154 μs
+*/
+
+gitgutter

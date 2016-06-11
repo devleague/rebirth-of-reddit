ade1ebe2b714b358a639ac23a8238d274acadb65
sgalutira <sheena.galutira@gmail.com>
2016-06-10 16:17:56 -1000

    created divs and vars for JSON data

--- app.js
+++ app.js
@@ -18,12 +18,41 @@ var posts = $.ajax({
   //Always update the UI with status
 });
 
-var redditData = posts.responseJSON.data.children;
-var author = redditData[i].data.author;
-var title = redditData[i].data.title;
-var forumText = redditData[i].data.selftext;
 
-var mainContainer = document.createElement('div');
-var headerDiv = document.createElement('div');
+    var redditData = posts.responseJSON.data.children;
+    var author = redditData[i].data.author;
+    var title = redditData[i].data.title;
+    var forumText = redditData[i].data.selftext;
+    var forumDate = redditData[i].data.created_utc;
 
 
+
+function newPosts(posts){
+  for (var i = 0; i < posts.length; i++){
+
+    var redditContainer = document.getElementById('reddit');
+    var mainContainer = document.createElement('div');
+    mainContainer.className = "subreddit";
+    redditContainer.appendChild(mainContainer);
+
+    var headerDiv = document.createElement('div');
+    headerDiv.className = "title";
+    headerDiv.innerHTML = title;
+    mainContainer.appendChild(headerDiv);
+
+    var bodyTextDiv = document.createElement('div');
+    bodyTextDiv.className = "forumText";
+    bodyTextDiv.innerHTML = forumText;
+    mainContainer.appendChild(bodyTextDiv);
+
+    var authorDiv = document.createElement('div');
+    authorDiv.className = "author";
+    authorDiv.innerHTML = "author";
+    mainContainer.appendChild(authorDiv);
+
+    var dateDiv = document.createElement('div');
+    dateDiv.className = "date";
+    dateDiv.innerHTML = forumDate;
+    mainContainer.appendChild(dateDiv);
+  }
+}

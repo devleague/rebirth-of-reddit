d90e12c6f9482a37ef1827a684fbe09d7ff7c09b
marunoa <marunoafp@gmail.com>
2016-06-11 14:51:10 -1000

    gulp-sass working!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require ('gulp');
+var sass = require ('gulp-sass');
 
-gulp.task ('upkeep', function () {
+gulp.task ('styles', function () {
+  gulp
+    .src ('scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+  })
+
+gulp.task ('watch', function () {
   gulp.watch('js/app.js', function () {
     console.log('The file changed');
     })

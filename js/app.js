df167cd5b45708f3308059ac3a961375d9cdccb5
Christie R <creindle@hawaii.edu>
2016-06-11 16:23:03 -1000

    MY TITLES HYPERLINKKKKKKK

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
 var postContainer = $("posts");
 var dataSet = $.ajax({
   method: 'GET',
-  url: 'https://www.reddit.com/r/LetsNotMeet.json',
+  url: 'https://www.reddit.com/r/NoSleep.json',
   dataType: 'json'
 })
 .done(function(data) {
 
   var allData = data.data.children;
 
+  var posts = document.createElement('div');
+
+  //var linkElement = document.createElement('a');
+
+
+
   // start post element
   // build out post element
   for (var i=0; i<allData.length;i++){
-    var postTitle = document.createElement('div');
-    postTitle.innerHTML = allData[i].data.title;
-    console.log(postTitle);
+    var postTitle = $('<h1 />');
+    //var postTitle = document.createElement('h1');
+    var linkElement = $('<a/>');
+    linkElement.attr('href', allData[i].data.url);
+    linkElement.text(allData[i].data.title);
+    //postTitle.innerHTML = allData[i].data.title;
+
+    //postTitle.url = allData[i].data.url;
     // append to post container
-    $("h1").append(postTitle.innerHTML);
+    $(postTitle).append(linkElement);
+    $("div").append(postTitle);
+    $(posts).append("div");
+
+    console.log(linkElement);
+    console.log(postTitle.innerHTML);
+    //$("div").append()
   }
 
 })

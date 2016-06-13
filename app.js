dd085413d1d477aa897ae0e22ccab2fdf039d31d
sgalutira <sheena.galutira@gmail.com>
2016-06-13 01:37:26 -1000

    added time stamp, changed fonts, and added main header

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ $.ajax({
   url: 'https://www.reddit.com/r/games.json',
   dataType: 'json'
 })
-.done(function(data) {
+.done(function(data1) {
   //handle successful response
-  newPosts(data);
+  newPosts(data1);
 
   // processResponse(data);
 })
@@ -18,6 +18,18 @@ $.ajax({
 });
 
   var redditContainer = document.getElementById('reddit');
+  var redditHeader = document.createElement('div');
+  redditHeader.id = "main-header";
+  redditContainer.appendChild(redditHeader);
+  var redditIcon = document.createElement('img');
+  redditIcon.src = "assets/reddit.png";
+  redditIcon.id = "reddit-img";
+  redditHeader.appendChild(redditIcon);
+  var spanElement = document.createElement('span');
+  spanElement.className = "span-header";
+  var redHeaderTxt = document.createTextNode("/r/Games");
+  spanElement.appendChild(redHeaderTxt);
+  redditHeader.appendChild(spanElement);
 
   function newPosts(response){
     var results = response.data.children;
@@ -27,7 +39,6 @@ $.ajax({
       //if forumText is truthy will return the forumText, if falsey returns title
       var forumText = results[i].data.selftext ? results[i].data.selftext : '<a href="' + titleLink + '">' +  titleLink +  '</a>';
       var numComments = results[i].data.num_comments;
-      var commentsText = results[i].data.body_html;
       var postDiv = postData('div', 'posts', null);
       redditContainer.appendChild(postDiv);
 
@@ -39,7 +50,7 @@ $.ajax({
       var bodyTextDiv = postData('div', 'forumText', forumText);
       postDiv.appendChild(bodyTextDiv);
 
-      var dateDiv = postData('div', 'date', redditDate(results));
+      var dateDiv = postData('div', 'date', redditDate(results) + ' ' + postTime(results));
       postDiv.appendChild(dateDiv);
 
       var authorDiv = postData('div', 'author', null);
@@ -47,7 +58,9 @@ $.ajax({
       authorDiv.appendChild(authorLink);
       postDiv.appendChild(authorDiv);
 
-      var commentDiv = postData('div', 'numComments', numComments);
+      var commentDiv = postData('div', 'numComments', null);
+      var commentLink = createLink('a', "https://www.reddit.com" + results[i].data.permalink, numComments + " comments");
+      commentDiv.appendChild(commentLink);
       postDiv.appendChild(commentDiv);
   }
   return redditContainer;
@@ -57,11 +70,34 @@ $.ajax({
     for (var i = 0; i < theDate.length; i++){
     var date = theDate[i].data.created_utc;
     var utcDate = new Date(date * 1000);
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    var month = ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
     return days[utcDate.getUTCDay()] + ', ' + month[utcDate.getUTCMonth()] + ' ' + utcDate.getUTCDate(date) + ', ' + utcDate.getUTCFullYear(date);
     }
   };
+
+  function postTime(time){
+    for (var i = 0; 1 < time.length; i++){
+    var utcTime = new Date(time[i].data.created_utc * 1000);
+    var hours = utcTime.getHours();
+    var minutes = utcTime.getMinutes();
+    var amOrPm = hours;
+      if (amOrPm >= 12){
+        amOrPm = 'pm';
+      }else{
+        amOrPm = 'am';
+      }
+    hours = hours % 12;
+    hours = hours % 12;
+      if (hours){
+        hours = hours;
+      }else{
+        hours = 12;
+      }
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    return hours + ':' + minutes + ' ' + amOrPm;
+    }
+  }
 
   function postData(element, className, text){
     var el = document.createElement(element);
